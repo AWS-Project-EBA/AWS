efeea93656be0d20e09085a87569bc84738c7ab4
sezena16 <32194484+sezena16@users.noreply.github.com>
2020-06-20 14:39:11 +0300

    Changes for navigation

--- app/src/main/java/com/example/rapunzel/NavActivity.kt
+++ app/src/main/java/com/example/rapunzel/NavActivity.kt
@@ -1,60 +1,112 @@
 package com.example.rapunzel
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.BaseAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_gridview.view.*
 import kotlinx.android.synthetic.main.activity_nav.*
 
+
 class NavActivity : AppCompatActivity() {
+    var adapter: TaAdapter? = null
+    var TasList = ArrayList<Tas>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav)
 
-        my_career_path.setOnClickListener {
-            val intent = Intent(this, CareerPathActivity::class.java)
-            startActivity(intent)
+        // load transactions
+        TasList.add(Tas("My Career Path", R.drawable.career))
+        TasList.add(Tas("My HR Manager", R.drawable.hr))
+        TasList.add(Tas("My Incoming Appointments", R.drawable.inap))
+        TasList.add(Tas("My Past Appointments",R.drawable.pastap))
+        TasList.add(Tas("My Inbox", R.drawable.inbox))
+        TasList.add(Tas("My Agenda", R.drawable.agenda))
+        TasList.add(Tas("My Financial Situation", R.drawable.finance))
+        TasList.add(Tas("My Services", R.drawable.services))
+        TasList.add(Tas("My Service Locations", R.drawable.sloc))
+        adapter = TaAdapter(this, TasList)
+
+        gvTas.adapter = adapter
+
+        gvTas.setOnItemClickListener(OnItemClickListener { parent, v, position, id ->
+            when (position) {
+                0 ->{
+                    val intent = Intent(this, CareerPathActivity::class.java)
+                    startActivity(intent)
+                }
+                1 ->{
+                    val intent = Intent(this, HRManagerActivity::class.java)
+                    startActivity(intent)
+                }
+                2 ->{
+                    val intent = Intent(this, IncomingAppActivity::class.java)
+                    startActivity(intent)
+                }
+                3 ->{
+                    val intent = Intent(this, PastAppActivity::class.java)
+                    startActivity(intent)
+                }
+                4 ->{
+                    val intent = Intent(this, InboxActivity::class.java)
+                    startActivity(intent)
+                }
+                5 ->{
+                    val intent = Intent(this, AgendaActivity::class.java)
+                    startActivity(intent)
+                }
+                6 ->{
+                    val intent = Intent(this, FinanceActivity::class.java)
+                    startActivity(intent)
+                }
+                7 ->{
+                    val intent = Intent(this, ServicesActivity::class.java)
+                    startActivity(intent)
+                }
+                8 ->{
+                    val intent = Intent(this, ServiceLocationsActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        })
+
+    }
+    class TaAdapter : BaseAdapter {
+        var TasList = ArrayList<Tas>()
+        var context: Context? = null
+
+        constructor(context: Context, TasList: ArrayList<Tas>) : super() {
+            this.context = context
+            this.TasList = TasList
         }
 
-        my_hr_manager.setOnClickListener {
-            val intent = Intent(this, HRManagerActivity::class.java)
-            startActivity(intent)
+        override fun getCount(): Int {
+            return TasList.size
         }
 
-        my_incoming_appointments.setOnClickListener {
-            val intent = Intent(this, IncomingAppActivity::class.java)
-            startActivity(intent)
+        override fun getItem(position: Int): Any {
+            return TasList[position]
         }
 
-        my_past_appointments.setOnClickListener {
-            val intent = Intent(this, PastAppActivity::class.java)
-            startActivity(intent)
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
         }
 
-        my_inbox.setOnClickListener {
-            val intent = Intent(this, InboxActivity::class.java)
-            startActivity(intent)
-        }
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val Ta = this.TasList[position]
 
-        my_agenda.setOnClickListener {
-            val intent = Intent(this, AgendaActivity::class.java)
-            startActivity(intent)
-        }
+            var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var TaView = inflator.inflate(R.layout.activity_gridview, null)
+            TaView.imgTa.setImageResource(Ta.image!!)
+            TaView.TaName.text = Ta.name!!
 
-        my_financial_situation.setOnClickListener {
-            val intent = Intent(this, FinanceActivity::class.java)
-            startActivity(intent)
+            return TaView
         }
-
-        my_services.setOnClickListener {
-            val intent = Intent(this, ServicesActivity::class.java)
-            startActivity(intent)
-        }
-
-        my_service_locations.setOnClickListener {
-            val intent = Intent(this, ServiceLocationsActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
---
+++ app/src/main/java/com/example/rapunzel/Tas.kt
@@ -0,0 +1,11 @@
+package com.example.rapunzel
+
+class Tas {
+    var name: String? = null
+    var image: Int? = null
+
+    constructor(name: String, image: Int) {
+        this.name = name
+        this.image = image
+    }
+}

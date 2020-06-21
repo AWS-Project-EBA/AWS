9811a7b4b0e99a651cd164fc7b3ff850431db689
Erayfelekk <erayfelek1@hotmail.com>
2020-06-21 22:01:47 +0300

    Agenda

--- app/src/main/java/com/example/rapunzel/AgendaActivity.kt
+++ app/src/main/java/com/example/rapunzel/AgendaActivity.kt
@@ -2,7 +2,20 @@ package com.example.rapunzel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_agenda.*
+import kotlinx.android.synthetic.main.agenda_row.view.*
+import kotlinx.android.synthetic.main.hr_row.view.*
+import okhttp3.*
+import java.io.IOException
 
 class AgendaActivity : AppCompatActivity() {
 
@@ -10,5 +23,87 @@ class AgendaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agenda)
         Log.d("Agenda" , "Agenda")
+
+        supportActionBar?.title = "HR Manager"
+
+        runOnUiThread {
+            fetchAgenda()
+        }
+
+        //fetchAgenda()
+
     }
+
+    fun fetchAgenda(){
+
+        val url = "https://l5lo98pz4j.execute-api.eu-central-1.amazonaws.com/prod/agenda?id=12"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback{
+
+            override fun onResponse(call: Call, response: Response) {
+
+                val body = response.body?.string()
+                Log.d("Rapu", body!!)
+
+                val gson = GsonBuilder().create()
+
+                val agenda =  gson.fromJson(body, Agenda::class.java)
+
+                var counter = agenda.date.size
+
+                Log.d("Rapu" , "size is: ${agenda.date.size}")
+
+                runOnUiThread( Runnable {
+                    kotlin.run {
+                        val adapter = GroupAdapter<ViewHolder>()
+
+                        for(i in 0 until counter){
+                            Log.d("Rapu" , "is is: ${agenda.date[i]}")
+                            adapter.add(AgendaItem( agenda.customer[i] , agenda.date[i] , agenda.hour[i] ) )
+                        }
+
+                        recyclerview_agenda.adapter = adapter
+                    }
+                })
+
+
+
+                //Log.d("Rapu" , agenda.dates!!.size.toString()   )
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute")
+            }
+
+        } )
+
+
+    }
+
+}
+
+class Agenda( val date: List<String> , val hour: List<String> , val customer: List<String> ) {
+
+}
+
+class AgendaItem(val customer: String , val date: String , val hour: String): Item<ViewHolder>() {
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+        Picasso.get().load(R.drawable.hr).into(viewHolder.itemView.imageView_agenda)
+
+        viewHolder.itemView.textView_agenda1.text = customer
+        viewHolder.itemView.textView_agenda2.text = date
+        viewHolder.itemView.textView_agenda3.text = hour
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.agenda_row
+    }
+
 }
--- app/src/main/java/com/example/rapunzel/HRManagerActivity.kt
+++ app/src/main/java/com/example/rapunzel/HRManagerActivity.kt
@@ -2,11 +2,78 @@ package com.example.rapunzel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_hrmanager.*
+import kotlinx.android.synthetic.main.hr_row.view.*
+import okhttp3.*
+import java.io.IOException
 
 class HRManagerActivity : AppCompatActivity() {
+
+    val adapter = GroupAdapter<ViewHolder>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hrmanager)
+
+        supportActionBar?.title = "HR Manager"
+
+
+        adapter.add(HRItem())
+        adapter.add(HRItem())
+        adapter.add(HRItem())
+        adapter.add(HRItem())
+
+        recyclerview_nav.adapter = adapter
+
+        fetchJson()
+
+    }
+
+    fun fetchJson(){
+        println("Attempting to fetch")
+
+        val url = "https://fg0l1o6kx3.execute-api.eu-central-1.amazonaws.com/prod/careerpath?id=12"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+
+                val body = response.body?.string()
+                Log.d("Rapu", body!!)
+
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute")
+            }
+
+        })
+
     }
 }
+
+class HRItem: Item<ViewHolder>() {
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+
+        Picasso.get().load(R.drawable.hr).into(viewHolder.itemView.imageView)
+
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.hr_row
+    }
+
+}
+
+
